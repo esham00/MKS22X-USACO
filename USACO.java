94433af2052a34c5069b8924c0256fc8aebbde73
Elizabeth Sham <esham00@stuy.edu>
2019-03-10 22:18:00 -0400

    added comments and commented out print statements

--- USACO.java
+++ USACO.java
@@ -1,10 +1,15 @@
 import java.util.*;
 import java.io.*;
 public class USACO {
+    //class for bronze
     private static class Bronzee {
+	//variables
     	private int R, C, E, N, R_s, C_s, D_s;
+	//stomping grounds
     	private int[][] grounds;
+	//scanner 
     	private Scanner grid;
+	//initializing everything
     	public Bronzee(String file) throws FileNotFoundException {
     	    File g = new File(file);
     	    Scanner gridz = new Scanner(g);
@@ -27,6 +32,7 @@ public class USACO {
     		e.printStackTrace();
     	    }
     	}
+	//getting the next move from scanner
 	public void nextMove() {
 	    if (grid.hasNextInt()) {
 		R_s = grid.nextInt()-1;
@@ -34,10 +40,12 @@ public class USACO {
 		D_s = grid.nextInt();
 	    }
 	}
+	//solving
 	public void solve(int instruction) {
+	    //if there's still moves 
 	    if (instruction < N) {
+		//find the highest ground
 		int max = 0;
-		int num = grounds[R_s][C_s];
 		for(int i = 0; i < 3; i++) {
 		    for(int j = 0; j < 3; j++) {
 			if (max < grounds[R_s+i][C_s+j]) {
@@ -45,6 +53,7 @@ public class USACO {
 			}
 		    }
 		}
+		//in the 3 by 3, if the ground within the highest ground by the depth then stomp it down to the max - depth
 		for(int i = 0; i < 3; i++) {
 		    for(int j = 0; j < 3; j++) {
 			if (max - grounds[R_s+i][C_s+j]<= D_s) {
@@ -52,14 +61,19 @@ public class USACO {
 			}
 		    }
 		}
+		//get the next set of instructions
 		nextMove();
+		//proceed until finished all instructions
 		solve(instruction+1);
 	    }
 	}
+	//calculating the volume of lake
 	public int volume() {
 	    int summation = 0;
+	    //go through the grounds
 	    for(int i = 0; i < grounds.length; i++) {
 		for(int j = 0; j < grounds[0].length; j++) {
+		    //finding the total depth
 		    grounds[i][j] = (grounds[i][j]*-1) + E;
 		    //System.out.println(toString());
 		    if (grounds[i][j] > 0) {
@@ -67,6 +81,7 @@ public class USACO {
 		    }
 		}
 	    }
+	    //multiplying it with 72 and 72
 	    return summation*72*72;
 	}
        public String toString() {
@@ -80,61 +95,82 @@ public class USACO {
 	    return output;
 	}
     }
+    //bronze
     public static int bronze(String filename) throws FileNotFoundException {
+	//instantiating class
 	Bronzee blank = new Bronzee(filename);
+	//solving
 	blank.solve(0);
+	//returning solution
 	return blank.volume();
     }
+    //silver
     public static int silver(String filename) throws FileNotFoundException{
 	try {
-	int[] possibleX = {0,0,-1,1};
-	int[] possibleY = {1,-1,0,0};
-	File f = new File(filename);
-	Scanner g = new Scanner(f);
-	int N = g.nextInt();
-	int M = g.nextInt();
-	int T = g.nextInt();
-	int[][] grid = new int[N][M];
-	for(int i = 0; i < N; i++) {
-	    String x = g.next();
-	    for(int j = 0; j < M; j++) {
-		if (x.charAt(j) == '.') {
-		    grid[i][j] = 0;
-		} else {
-		    grid[i][j] = -1;
-		}
-	    }
-	}
-	//	System.out.println(toString(grid));
-	int R1 = g.nextInt()-1;
-	int C1 = g.nextInt()-1;
-	int R2 = g.nextInt()-1;
-	int C2 = g.nextInt()-1;
-	grid[R1][C1] = 1;
-	for(int i = 0; i < T; i++) {
-	    int[][]gridz = new int[N][M];
-	    for(int x = 0; x <  grid.length; x++) {
-		for(int y = 0; y < grid[0].length; y++) {
-		    if (grid[x][y] == -1) {
-			gridz[x][y] = -1;
+	    //possible moves
+	    int[] possibleX = {0,0,-1,1};
+	    int[] possibleY = {1,-1,0,0};
+	    //reading from file
+	    File f = new File(filename);
+	    Scanner g = new Scanner(f);
+	    int N = g.nextInt();
+	    int M = g.nextInt();
+	    int T = g.nextInt();
+	    //making the array
+	    int[][] grid = new int[N][M];
+	    for(int i = 0; i < N; i++) {
+		//basically the line since the dots have no space
+		String x = g.next();
+		for(int j = 0; j < M; j++) {
+		    //. = 0
+		    if (x.charAt(j) == '.') {
+			grid[i][j] = 0;
 		    } else {
-			int count = 0;
-			for(int z = 0; z < 4; z++) {
-			    int row = x + possibleX[z];
-			    int col = y + possibleY[z];
-			    if (check(row,col,grid)) {
-				count += grid[row][col];
-			    }
-			}
-			gridz[x][y] = count;
+			//* = -1 
+			grid[i][j] = -1;
 		    }
 		}
 	    }
-	    grid = gridz;
-	    System.out.println(toString(gridz));
-	}
-	
-	return grid[R2][C2];
+	    //	System.out.println(toString(grid));
+	    //getting the start coordinates
+	    int R1 = g.nextInt()-1;
+	    int C1 = g.nextInt()-1;
+	    //getting the end coordinates
+	    int R2 = g.nextInt()-1;
+	    int C2 = g.nextInt()-1;
+	    //setting the start to be 1
+	    grid[R1][C1] = 1;
+	    //looping through the amount of time
+	    for(int i = 0; i < T; i++) {
+		//creating a second grid(temporary)
+		int[][]gridz = new int[N][M];
+		//going through the grid
+		for(int x = 0; x <  grid.length; x++) {
+		    for(int y = 0; y < grid[0].length; y++) {
+			//place the trees in the temp grid
+			if (grid[x][y] == -1) {
+			    gridz[x][y] = -1;
+			} else {
+			    //variable to keep track of the values
+			    int count = 0;
+			    //going through the possible moves
+			    for(int z = 0; z < 4; z++) {
+				int row = x + possibleX[z];
+				int col = y + possibleY[z];
+				//if u can utilize the possible moves add it to the variable
+				if (check(row,col,grid)) {
+				    count += grid[row][col];
+				}
+			    }
+			    gridz[x][y] = count;
+			}
+		    }
+		}
+		//updating grid
+		grid = gridz;
+		//System.out.println(toString(gridz));
+	    }
+	    return grid[R2][C2];
 	} catch (Exception e) {
 	    e.printStackTrace();
 	}
@@ -150,7 +186,7 @@ public class USACO {
 	}
 	return output;
     }
-	       
+    //checking to see if you can add at row and col
     public static boolean check(int row, int col, int[][] grid) {
 	if (row >= 0 && row < grid.length && col >= 0 && col < grid[0].length && grid[row][col] != -1) {
 	    return true;
@@ -158,10 +194,10 @@ public class USACO {
 	    return false;
 	}
     }
-    public static void main(String[] args)throws FileNotFoundException {
-	System.out.println(bronze("makeLake.txt"));
-	System.out.println(silver("silverTest.txt"));
-    }
+    // public static void main(String[] args)throws FileNotFoundException {
+    // 	System.out.println(bronze("makeLake.txt"));
+    // 	System.out.println(silver("silverTest.txt"));
+    // }
 }
 	
 	
